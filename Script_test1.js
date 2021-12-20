e54a862b204293676df9ea1b99c0af9cb22a5894
dodo88c <dodo88c@gmail.com>
2021-12-20 06:41:17 +0100

    Update Script_test1.js

--- Script_test1.js
+++ Script_test1.js
@@ -1,3 +1,2 @@
 
-
-alert("Hello Geeks");
+document.querySelector("body > ui-layout > div > et-layout-main > div > div.container-level2.layout-sidenav-expanded.layout-sidenav-closed > div.container-level3 > div.page-content > div > ng-view > div > div.w-portfolio-table.portfolio-page-type1-content-body.portfolio-open-trades-inner.head > div > div > div.e-btn.light.i-ptc-action.edit-extended.dropdown-menu.ng-scope.ng-isolate-scope.open > div > div > trade-button > div").clic()
