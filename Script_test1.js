c3c989389d90505e0a767123af9461ad266676bc
dodo88c <dodo88c@gmail.com>
2021-12-20 06:45:33 +0100

    Update Script_test1.js

--- Script_test1.js
+++ Script_test1.js
@@ -1,2 +1,17 @@
 
-document.querySelector("body > ui-layout > div > et-layout-main > div > div.container-level2.layout-sidenav-expanded.layout-sidenav-closed > div.container-level3 > div.page-content > div > ng-view > div > div.w-portfolio-table.portfolio-page-type1-content-body.portfolio-open-trades-inner.head > div > div > div.e-btn.light.i-ptc-action.edit-extended.dropdown-menu.ng-scope.ng-isolate-scope.open > div > div > trade-button > div").clic()
+
+
+for (let step = 0; step < 5; step++) {
+
+  sleep(1000)  
+  .then(() => document.querySelector("body > ui-layout > div > et-layout-main > div > div.container-level2.layout-sidenav-expanded.layout-sidenav-closed > div.container-level3 > div.page-content > div > ng-view > div > div.w-portfolio-table.portfolio-page-type1-content-body.portfolio-open-trades-inner.head > div > div > div.e-btn.light.i-ptc-action.edit-extended.dropdown-menu.ng-scope.ng-isolate-scope.open > div > div > trade-button > div").click();
+  .then(() => sleep(1000);
+  .then(() => document.querySelector("#open-position-view > div.execution-head > div.execution-head-buttons.ng-scope > button:nth-child(1)").click();
+  .then(() => sleep(1000);
+  .then(() => document.querySelector("#open-position-view > div.execution-head > div.head-buttons-ph > a").click();
+
+}
+
+
+
+
